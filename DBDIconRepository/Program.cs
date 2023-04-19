e82c3c1d9c897e2c0fc9f3ccb31fbca6d0e51833
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2023-04-19 15:25:04 +0700

    Single instance

---
+++ DBDIconRepository/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.UI.Dispatching;
+using Microsoft.Windows.AppLifecycle;
+using Windows.ApplicationModel.Activation;
+using Windows.Storage;
+
+namespace DBDIconRepository;
+
+public class Program
+{
+    [STAThread]
+    static async Task<int> Main(string[] args)
+    {
+        WinRT.ComWrappersSupport.InitializeComWrappers();
+        bool isRedirect = await DecideRedirection();
+        if (!isRedirect)
+        {
+            Microsoft.UI.Xaml.Application.Start((p) =>
+            {
+                var context = new DispatcherQueueSynchronizationContext(
+                    DispatcherQueue.GetForCurrentThread());
+                SynchronizationContext.SetSynchronizationContext(context);
+                new App();
+            });
+        }
+
+        return 0;
+    }
+
+    private static string AppUUID = "81e0be52-6d08-408d-938a-75326d0f72a7";
+    private static async Task<bool> DecideRedirection()
+    {
+        bool isRedirect = false;
+        AppActivationArguments args = AppInstance.GetCurrent().GetActivatedEventArgs();
+        ExtendedActivationKind kind = args.Kind;
+        AppInstance keyInstance = AppInstance.FindOrRegisterForKey(AppUUID);
+
+        if (keyInstance.IsCurrent)
+        {
+            keyInstance.Activated += OnActivated;
+        }
+        else
+        {
+            isRedirect = true;
+            await keyInstance.RedirectActivationToAsync(args);
+        }
+        return isRedirect;
+    }
+
+    private static void OnActivated(object sender, AppActivationArguments args)
+    {
+        ExtendedActivationKind kind = args.Kind;
+    }
+}
